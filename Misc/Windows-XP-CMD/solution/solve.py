19e670533b98d6384b1def8ce81676201d375f1a
supasuge <158092262+supasuge@users.noreply.github.com>
2026-01-14 01:25:48 -0500

    Update solve.py to provide flag retrieval instructions

--- Misc/Windows-XP-CMD/solution/solve.py
+++ Misc/Windows-XP-CMD/solution/solve.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def solve():
-    print("Solve script for Misc challenge 1 goes here.")
+    print("just run the following:\ntype C:\Administrator\Desktop\DocumentsandSettings\secret\dont_look_here\seriously\flag.txt")
+    print("It should return the flag...")
+    # good nuff
 
 if __name__ == "__main__":
     solve()
