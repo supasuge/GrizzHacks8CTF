e60694324bb08b2553a61a85aac89e5a59e3aba6
supasuge <158092262+supasuge@users.noreply.github.com>
2026-03-27 03:16:35 +0000

    Remove humorous echo statement from setup.sh
    Happens

--- Web/Aegis_Relay/src/setup.sh
+++ Web/Aegis_Relay/src/setup.sh
@@ -26,5 +26,3 @@ echo ""
 echo "To run with Docker:"
 echo "  docker compose up --build -d"
 echo ""
-
-echo "RUUUUUNNNNN ITTTTT MY DOGGIE (she calling me daddy, 350 that's patek. 500 that's RICH. 250 so SKELLY MANE IM SO TIRED OF THIS SHIT)"
